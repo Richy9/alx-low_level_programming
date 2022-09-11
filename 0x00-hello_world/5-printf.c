f7cbfa8c12a64585b479d56a931435f4862735ad
Richy9 <you@example.com>
2022-09-11 15:54:31 -0700

    My Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Description: main - entry string to stdout
+ *
+ * Return: 0 (if successful)
+ */
+int main(void)
+{
+printf("%s\n","with proper grammer, but the outcome is a piece of art");
+return(0);
+
+}
+
