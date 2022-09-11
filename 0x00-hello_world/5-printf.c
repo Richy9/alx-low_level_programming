f5d19bca346ea578a220fea3f78dca4539243c37
Richy9 <you@example.com>
2022-09-11 16:01:40 -0700

    My Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-printf("%s\n", "with proper grammer, but the outcome is a piece of art");
+printf("%s\n", "with proper grammer, but the outcome is a piece of art,");
 return (0);
 
 }
