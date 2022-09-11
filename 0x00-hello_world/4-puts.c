b95f28ebde46e412511def751ad492368040fe89
Richy9 <you@example.com>
2022-09-11 15:24:54 -0700

    My Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Description: main - entry print string to stdout
+ *
+ * Return: 0 (if successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+	Return (0);
+}
+
